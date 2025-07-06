d6ea4de817a3c29adc4a3c45a24391ebb97d74e3
jack <logan676395@gmail.com>
2025-07-06 20:08:48 +0800

    Add Text to friends screen

--- feature/friends/src/main/java/com/puskal/friends/FriendsScreen.kt
+++ feature/friends/src/main/java/com/puskal/friends/FriendsScreen.kt
@@ -3,6 +3,7 @@ package com.puskal.friends
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
