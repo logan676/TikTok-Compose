6613858455f190916c566b0e75cd6c3546a441f5
jack <logan676395@gmail.com>
2025-07-20 19:56:33 +0800

    Fix build error

--- feature/cameramedia/src/main/java/com/puskal/cameramedia/tabs/CameraScreen.kt
+++ feature/cameramedia/src/main/java/com/puskal/cameramedia/tabs/CameraScreen.kt
@@ -1,5 +1,6 @@
 package com.puskal.cameramedia.tabs
 
+import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
