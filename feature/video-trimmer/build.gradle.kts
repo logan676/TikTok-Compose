ff1b3569eda600f29ce47b1d6210698eadc8176b
Jack <logan676@163.com>
2025-07-21 21:13:37 +0800

    Convert video-trimmer module to library

--- feature/video-trimmer/build.gradle.kts
+++ feature/video-trimmer/build.gradle.kts
@@ -1,53 +1,8 @@
-import AppConfig
-import Libraries
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.redevrx.video_trimmer"
-    compileSdk = AppConfig.compileSdk
-
-    defaultConfig {
-        applicationId = "com.redevrx.video_trimmer"
-        minSdk = AppConfig.minSdk
-        targetSdk = AppConfig.targetSdk
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+    id("plugin.android-common")
 }
 
 dependencies {
-
-    implementation(Libraries.AndroidX.coreKtx)
-    implementation(Libraries.AndroidX.appCompat)
-    implementation(Libraries.Google.material)
     media3Dependency()
     implementation("com.github.CanHub:Android-Image-Cropper:4.3.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation(Libraries.Test.junitExtKtx)
-    androidTestImplementation(Libraries.Test.espressorCore)
-    androidTestImplementation(Libraries.Test.testCoreKtx)
-    androidTestImplementation(Libraries.Test.runner)
 }
