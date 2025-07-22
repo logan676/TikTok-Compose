0e4ac4beb32f5b9e885f0e15137b23bad1be7a15
Jack <logan676@163.com>
2025-07-22 22:02:38 +0800

    feat: add expandable toolbar in video editor

--- feature/cameramedia/src/main/java/com/puskal/cameramedia/edit/VideoEditContract.kt
+++ feature/cameramedia/src/main/java/com/puskal/cameramedia/edit/VideoEditContract.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.InvertColors
 import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.filled.TextFields
@@ -28,15 +29,16 @@ enum class VideoEditTool(@StringRes val title: Int, val icon: ImageVector) {
     CHALLENGE(R.string.challenge, Icons.Filled.Flag),
     STICKERS(R.string.stickers, Icons.Filled.EmojiEmotions),
     EFFECTS(R.string.effects, Icons.Filled.Wallpaper),
+    MORE(R.string.see_more, Icons.Filled.KeyboardArrowUp),
     FILTERS(R.string.filters, Icons.Filled.InvertColors),
     BEAUTY(R.string.beauty, Icons.Filled.Face),
     CROP_RESIZE(R.string.crop_resize, Icons.Filled.Crop)
 }
 
-enum class ResizeMenuFeature(val icon: ImageVector) {
-    AUTO_SUBTITLES(Icons.Outlined.Subtitles),
-    QUALITY_ENHANCEMENT(Icons.Outlined.Star),
-    VOICE_CHANGER(Icons.Outlined.Mic),
-    BRUSH_TOOL(Icons.Outlined.Brush),
-    COLLAPSE_TOOLBAR(Icons.Filled.KeyboardArrowDown)
+enum class ResizeMenuFeature(@StringRes val title: Int, val icon: ImageVector) {
+    AUTO_SUBTITLES(R.string.audio_transcript, Icons.Outlined.Subtitles),
+    QUALITY_ENHANCEMENT(R.string.enhance_video_quality, Icons.Outlined.Star),
+    VOICE_CHANGER(R.string.change_voice, Icons.Outlined.Mic),
+    BRUSH_TOOL(R.string.drawing_painter, Icons.Outlined.Brush),
+    COLLAPSE_TOOLBAR(R.string.hide, Icons.Filled.KeyboardArrowDown)
 }
